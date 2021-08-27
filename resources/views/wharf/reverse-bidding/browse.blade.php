07cba7dd450ef8e11a2dec7d3c2f16397299f4fc
Earl <empicearl@gmail.com>
2021-08-27 11:09:07 +0800

    test

--- resources/views/wharf/reverse-bidding/browse.blade.php
+++ resources/views/wharf/reverse-bidding/browse.blade.php
@@ -112,7 +112,6 @@
             <div class="toast-header">
                 <i class="fa fa-cart-plus"> </i>
                 <strong class="mr-auto m-l-sm">Add to Cart</strong>
-{{--                <small>2 seconds ago</small>--}}
                 <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
