96ff5860982f133102400bcc0b7dd902b5c36726
Angela Nicole Belen <anbelen.official@gmail.com>
2021-10-06 13:06:12 +0800

    update wharf ui

--- resources/views/wharf/market-place/my_orders.blade.php
+++ resources/views/wharf/market-place/my_orders.blade.php
@@ -61,9 +61,10 @@
                                                             <div class="column-1 d-flex align-items-center">
                                                                 <a href="{{route('market-place.show', $product->id)}}">
                                                                     <div class="cart-product-imitation">
+                                                                        {!! $image !!}
                                                                         {{--                                                    {!! ($cartItem->hasMedia('market-place')? "<img class='img-thumbnail' src='".url('/').$cartItem->getFirstMediaUrl('market-place')."'>":'')  !!}--}}
                                                                         {{--<div class="img" style="background-image: url({!! $image !!})"></div>--}}
-                                                                        <div class="img" style="background-image: url({!! ($product->hasMedia('market-place')? url('/').$product->getFirstMediaUrl('market-place'):'')  !!})"></div>
+                                                                        {{--<div class="img" style="background-image: url({!! ($product->hasMedia('market-place')? url('/').$product->getFirstMediaUrl('market-place'):'')  !!})"></div>--}}
 
                                                                     </div>
                                                                 </a>
